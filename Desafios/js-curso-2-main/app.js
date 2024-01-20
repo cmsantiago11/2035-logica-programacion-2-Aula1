2b8a658ee90b9635fd58a17f3f1593713693e12e
Santiago_Calle <cmsantiago32@gmail.com>
2024-01-20 13:50:53 -0500

    Primer desafio completo

--- Desafios/js-curso-2-main/app.js
+++ Desafios/js-curso-2-main/app.js
@@ -1,1 +1,25 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Hora del desafío';
 
+//let parrafo = document.querySelector('p');
+//parrafo.innerHTML = 'Indica un número del 1 al 10';
+
+function intentoDeUsuario() {
+    alert('Has hecho click desde la funcion');
+}
+
+function preguntaCiudadBrasil() {
+    let ciudadBrasil = prompt('¿Nombre de una ciudad de Brasil?');
+    alert(`Estuve en ${ciudadBrasil} y me acordé de ti`);
+}
+
+function alertaJS() {
+    alert(`Yo amo JS`);
+}
+
+function suma() {
+    let numero1 = parseInt(prompt('Ingrese un número'));
+    let numero2 = parseInt(prompt('Ingrese otro número'));
+    suma2 = numero1 + numero2;
+    alert(`la suma de ambos números es: ${suma2}`);
+}
