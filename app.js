b15ed63c0085270f9094b2ce669208e9bb596336
Santiago_Calle <cmsantiago32@gmail.com>
2024-01-20 13:34:03 -0500

    Uso de onclick con funcion en JS

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ titulo.innerHTML = 'Juego del número secreto';
 
 let parrafo = document.querySelector('p');
 parrafo.innerHTML = 'Indica un número del 1 al 10';
+
+function intentoDeUsuario() {
+    alert('Has hecho click desde la funcion');
+}
