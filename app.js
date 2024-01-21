6e9b07fd648490e0cc3176af83d92684b2026b41
Santiago_Calle <cmsantiago32@gmail.com>
2024-01-20 20:50:52 -0500

    Funciones con return

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Juego del número secreto';
+let numeroSecreto = generarNumeroSecreto();
+let intentos = 0;
 
-let parrafo = document.querySelector('p');
-parrafo.innerHTML = 'Indica un número del 1 al 10';
+console.log(numeroSecreto);
 
-function intentoDeUsuario() {
-    alert('Has hecho click desde la funcion');
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
 }
+
+function verificarIntento() {
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    
+    if (numeroDeUsuario === numeroSecreto) {
+        console.log('Acertaste el número!');
+    } 
+    return;
+}
+
+
+function generarNumeroSecreto() {
+    return Math.floor(Math.random()*10)+1;
+
+}
+
+asignarTextoElemento('h1','Juego del número secreto!');
+asignarTextoElemento('p',`Indica un número del 1 al 10`);
